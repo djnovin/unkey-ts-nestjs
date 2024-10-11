9ba27a7dbbce041743d9372fb5932316fe1e4deb
djnovin <hello@novinnoori.com>
2024-10-11 15:14:32 +1100

    Initialise Nest JS Project

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { AppController } from './app.controller';
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
