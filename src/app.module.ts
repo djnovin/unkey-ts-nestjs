5635ef1ff0b859bba1f56d2dea6ce7fda3de1dc1
djnovin <hello@novinnoori.com>
2024-10-13 13:51:27 +1100

    Import AuthGuard to App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
+import { UnkeyModule } from './unkey/unkey.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UnkeyModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthGuard],
 })
 export class AppModule {}
